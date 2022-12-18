49f43d3b6f721a9b61c5c4e0536c73c46b77aef2
notanerdstudent <akshutgoyal2301@gmail.com>
2022-12-18 12:45:51 +0530

    Improvements, Bug Fixes

---
+++ animations/title.js
@@ -0,0 +1,17 @@
+var msg = document.title;
+var speed = 500;
+var endChar = "... ";
+var pos = 0;
+
+function moveTitle() {
+  var ml = msg.length;
+
+  title = msg.substring(pos, ml) + endChar + msg.substring(0, pos);
+  document.title = title;
+
+  pos++;
+  if (pos > ml) pos = 0;
+  window.setTimeout("moveTitle()", speed);
+}
+
+moveTitle();
