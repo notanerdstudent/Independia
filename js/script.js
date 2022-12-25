f766836c6c631763192058b7f9e030a271a110a7
notanerdstudent <akshutgoyal2301@gmail.com>
2022-12-25 22:14:00 +0530

    Section 3 Updated

--- js/script.js
+++ js/script.js
@@ -21,3 +21,35 @@ AOS.init({
 const year = document.getElementById("year-copy");
 
 year.innerHTML = new Date().getFullYear();
+
+// ? Swiper
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    200: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
